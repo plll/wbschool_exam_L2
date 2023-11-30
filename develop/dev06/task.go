e28eb8428f2af540a61246877dffeb7c4b412955
plll <lieonid2003@yandex.ru>
2023-11-30 18:07:13 +0300

    develop/6

--- develop/dev06/task.go
+++ develop/dev06/task.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
 /*
 === Утилита cut ===
 
@@ -13,6 +22,56 @@ package main
 Программа должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
-func main() {
+type fieldsArr []int
 
+func (i *fieldsArr) String() string {
+	return fmt.Sprintf("%v", *i)
+}
+
+func (i *fieldsArr) Set(value string) error {
+	for _, dt := range strings.Split(value, ",") {
+		tmp, err := strconv.Atoi(dt)
+		if err != nil {
+			return err
+		}
+		*i = append(*i, tmp)
+	}
+	return nil
+}
+
+func main() {
+	var fields fieldsArr
+	var separated bool
+	var delimiter string
+	flag.Var(&fields, "f", "Номера колонок")
+	flag.StringVar(&delimiter, "d", ",", "Разделитель")
+	flag.BoolVar(&separated, "s", false, "Только строки с разделителем")
+	flag.Parse()
+
+	rows := make([][]string, 0)
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		row := scanner.Text()
+		if row == "" {
+			break
+		}
+		rowFields := strings.Split(row, delimiter)
+		rows = append(rows, rowFields)
+	}
+
+	fieldsAmount := len(rows[0])
+	for _, row := range rows {
+		var output string
+		if separated && len(row) != fieldsAmount {
+			continue
+		}
+		for _, x := range fields {
+			if output == "" {
+				output += row[x]
+			} else {
+				output = output + delimiter + row[x]
+			}
+		}
+		fmt.Println(output)
+	}
 }
